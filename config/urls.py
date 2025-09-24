7a77276b78101c77bd4a9b3d090664307e3d5f49
Manish Bhusal <manishbhusal6@gmail.com>
2025-09-24 11:45:24 +0545

    Enforce authentication on subdomains: redirect unauthenticated users to login

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include
 from django.urls import path
@@ -8,7 +9,11 @@ from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path(
+        "",
+        login_required(TemplateView.as_view(template_name="pages/home.html")),
+        name="home",
+    ),
     path(
         "about/",
         TemplateView.as_view(template_name="pages/about.html"),
